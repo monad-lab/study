fbc94c0cddc2ffd512f380fcfdfd5d148df3e045
andrew <amuro.june@gmail.com>
2024-03-02 12:13:32 +0900

    update ch7 instance Functor for MyList

--- ch7.hs
+++ ch7.hs
@@ -5,18 +5,17 @@ codelab = error "NotImplemented"
 ------------------------------------
 data MyList a = Nil | Cons a (MyList a) deriving (Show)
 
+instance Functor MyList where
+  fmap f Nil = Nil
+  fmap f (Cons x xs) = Cons (f x) (fmap f xs)
+
 mymap f Nil = Nil
 mymap f (Cons a b) = Cons (f a) (mymap f b)
 
--- FIXME: error. Couldn't match type 'a' with 'b'
-instance Functor MyList where
-  fmap f Nil = Nil
-  fmap f (Cons x y) = codelab
   
 main :: IO ()
 main = do
   print $ "chap7"
   print $ fmap (+1) [1,2,3]                         -- [2,3,4]
   print $ mymap (+1) (Cons 1 (Cons 2 (Cons 3 Nil)))  --Cons 2 (Cons 3 (Cons 4 Nil))
-  -- FIXME
-  --print $ fmap (+1) (Cons 1 (Cons 2 (Cons 3 Nil)))
+  print $ fmap (+1) (Cons 1 (Cons 2 (Cons 3 Nil)))
